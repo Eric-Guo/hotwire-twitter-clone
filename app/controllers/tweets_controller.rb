58007365ad85f13bc37b8d34bfac91de9635fd0e
Eric-Guo <eric.guocz@gmail.com>
2021-10-19 10:28:57 +0800

    Remove no use comments.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,29 +1,21 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
-  # GET /tweets
-  # GET /tweets.json
   def index
     @tweets = Tweet.all
     @tweet = Tweet.new
   end
 
-  # GET /tweets/1
-  # GET /tweets/1.json
   def show
   end
 
-  # GET /tweets/new
   def new
     @tweet = Tweet.new
   end
 
-  # GET /tweets/1/edit
   def edit
   end
 
-  # POST /tweets
-  # POST /tweets.json
   def create
     @tweet = Tweet.new(tweet_params)
 
@@ -39,8 +31,6 @@ class TweetsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /tweets/1
-  # PATCH/PUT /tweets/1.json
   def update
     respond_to do |format|
       if @tweet.update(tweet_params)
@@ -53,8 +43,6 @@ class TweetsController < ApplicationController
     end
   end
 
-  # DELETE /tweets/1
-  # DELETE /tweets/1.json
   def destroy
     @tweet.destroy
     respond_to do |format|
@@ -64,12 +52,11 @@ class TweetsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_tweet
       @tweet = Tweet.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def tweet_params
       params.require(:tweet).permit(:body, :likes_count, :retweets_count)
     end
