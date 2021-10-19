2670caf873a5f44e980758c3eabbf5361d248a88
Eric-Guo <eric.guocz@gmail.com>
2021-10-19 13:34:30 +0800

    Better way.

--- app/controllers/double_turbos_controller.rb
+++ app/controllers/double_turbos_controller.rb
@@ -1,9 +1,6 @@
 class DoubleTurbosController < ApplicationController
   def show
     @turbo_time = Time.now
-    s = render_to_string partial: 'double_turbos/link', locals: { turbo_time: @turbo_time }
-    p1 = turbo_stream.replace 'double_turbo_part1', s
-    p2 = turbo_stream.replace 'double_turbo_part2', "<turbo-frame id='double_turbo_part2'>#{@turbo_time}</turbo-frame>"
-    render turbo_stream: [p1, p2]
+    render content_type: 'text/vnd.turbo-stream.html'
   end
 end
