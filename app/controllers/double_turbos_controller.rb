f29d8bf91751d2e844edec38c81dbb52b9643b6a
Eric-Guo <eric.guocz@gmail.com>
2021-10-19 11:55:06 +0800

    Support double turbo links.

--- app/controllers/double_turbos_controller.rb
+++ app/controllers/double_turbos_controller.rb
@@ -1,5 +1,9 @@
 class DoubleTurbosController < ApplicationController
   def show
     @turbo_time = Time.now
+    s = render_to_string partial: 'double_turbos/link', locals: { turbo_time: @turbo_time }
+    p1 = turbo_stream.replace 'double_turbo_part1', s
+    p2 = turbo_stream.replace 'double_turbo_part2', "<turbo-frame id='double_turbo_part2'>#{@turbo_time}</turbo-frame>"
+    render turbo_stream: [p1, p2]
   end
 end
